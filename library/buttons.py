92bdcd9cd410ab5a9b3f412699b22a818abb0c5f
Doalendikoo <96226832+Doalendikoo@users.noreply.github.com>
2021-12-18 21:03:07 +0630

    Update buttons.py

--- library/buttons.py
+++ library/buttons.py
@@ -21,7 +21,7 @@ home_button = [
 
 
 start_button = [
-    [InlineKeyboardButton("🏅 Telegram 🏅", url="@howtodoyt"),
+    [InlineKeyboardButton("🏅 Telegram 🏅", url=" @lawliet_the_l"),
      InlineKeyboardButton("⚙️Settings ⚙", "start_btn")]
 ]
 
@@ -49,7 +49,7 @@ finished_button = [
 
 
 terminate_btn = [
-    [InlineKeyboardButton("🧸 Updates", url="@howtodoyt"),
+    [InlineKeyboardButton("🧸 Updates", url=" @lawliet_the_l"),
      InlineKeyboardButton("❓ Usage", url="https://telegra.ph/Clonebot-UI-Help-05-30")],
     [InlineKeyboardButton("🚫 Terminate", "terminate_btn"),
      InlineKeyboardButton("🏠 Home", "start_btn")]
